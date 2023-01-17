539e7d342cd7918aca126b7eaa5076b8efa0b119
b.gue.mquez <b.gue.mquez@gmail.com>
2023-01-16 20:55:12 -0600

    Add project files.

---
+++ CalculoMasaCorporal/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿
+namespace CalculoMasaCorporal
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textPeso = new System.Windows.Forms.TextBox();
+            this.textAltura = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(398, 254);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(147, 67);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Calcular";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ControlText;
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(188, 97);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(147, 32);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Peso  (Kg)";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.ControlText;
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label2.Location = new System.Drawing.Point(188, 166);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(138, 32);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Altura (m)";
+            // 
+            // textPeso
+            // 
+            this.textPeso.Location = new System.Drawing.Point(398, 91);
+            this.textPeso.Name = "textPeso";
+            this.textPeso.Size = new System.Drawing.Size(135, 38);
+            this.textPeso.TabIndex = 3;
+            // 
+            // textAltura
+            // 
+            this.textAltura.Location = new System.Drawing.Point(398, 166);
+            this.textAltura.Name = "textAltura";
+            this.textAltura.Size = new System.Drawing.Size(135, 38);
+            this.textAltura.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.ControlText;
+            this.label3.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label3.Location = new System.Drawing.Point(163, 27);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(450, 32);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Calcula tu indice de masa corporal";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(761, 399);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textAltura);
+            this.Controls.Add(this.textPeso);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Calculo de Masa Corporal";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textPeso;
+        private System.Windows.Forms.TextBox textAltura;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ CalculoMasaCorporal/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalculoMasaCorporal
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string label1 = textPeso.Text;
+            int peso = Int32.Parse(label1);
+           
+            string label2 = textAltura.Text;
+            double altura = double.Parse(label2);
+
+            double imc = peso / (altura * altura);
+
+            MessageBox.Show("El indice de masa corporal es de : " + imc);
+
+        }
+    }
+}
